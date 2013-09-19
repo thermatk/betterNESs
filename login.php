a6cfe668588dde21f32d35ea45649341c5836799
thermatk <thermatk@thermatk.com>
2013-09-20 00:44:28 +0400

    Basic login page template!

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- <link rel="shortcut icon" href="/assets/ico/favicon.png"> -->
+
+    <title>Войти в betterNESs</title>
+
+    <link href="/vendor/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/signin.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Войдите</h2>
+        <input type="text" class="form-control" placeholder="Адрес email" autofocus>
+        <input type="password" class="form-control" placeholder="Пароль">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Запомнить меня
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+      </form>
+
+      <hr>
+      <footer>
+        <p>&copy; thermatk 2013</p>
+      </footer>
+    </div>
+
+    <script src="vendor/jquery-1.10.2.min.js"></script>
+    <script src="vendor/bootstrap/bootstrap.min.js"></script>
+  </body>
+</html>
