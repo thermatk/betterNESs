2b9422439b89ea31624abf3aef289217b8167554
thermatk <thermatk@thermatk.com>
2013-09-20 10:06:45 +0400

    change admin const before it's too late

--- php/ThUser.class.php
+++ php/ThUser.class.php
@@ -2,7 +2,7 @@
 require_once("vendor/class.uFlex.php");
 class ThUser extends uFlex{
     public function isAdmin() {
-        if($this->data['group_id'] == 1) {
+        if($this->data['group_id'] == 0) {
             return true;
         } else {
             return false;
