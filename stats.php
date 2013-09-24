f45b8a138dafe06648ef1f4b28d6516cae802d17
thermatk <thermatk@thermatk.com>
2013-09-25 00:02:49 +0400

    stats page ready

--- stats.php
+++ stats.php
@@ -6,7 +6,108 @@ require("header.php");
 	<h1>Посмотреть оценки</h1>
 </div>
 <div class="row">
-N/A yet :D
+<?php
+	$q=$pdodb->query('SELECT * from tasks ORDER BY task_id DESC;');
+	$q->setFetchMode(PDO::FETCH_ASSOC);
+	while($task = $q->fetch()) {
+		echo "<h3>".$task["task_id"].". ".$task["taskname"]."</h3>";
+		$criteria = json_decode($task['criterias']);
+	?>
+		<table class="table table-bordered table-striped">
+			<thead>
+	          <tr>
+	          	<?php 
+	          	if ($user->isAdmin()) {
+	          		echo "<th>#</th>";	          		
+	          		echo "<th>Имя</th>";
+	          	}
+	          	foreach ($criteria as $key => $value) {
+	          		$value = get_object_vars($value);
+	          		echo "<th>".$value['name'].", баллов</th>";
+	          		echo "<th>".$value['name'].", комментарий</th>";
+	          	?>
+	            	
+	            <?php
+	        	}
+	        	?>
+	            <th>Сумма баллов</th>
+	          </tr>
+	        </thead>
+        	<tbody>
+			<?php
+			if (!($user->isAdmin())) {
+				$q3=$pdodb->query('SELECT * from grades WHERE task_id = "'.$task['task_id'].'" AND user_id = "'.$user->id.'";');
+				$q3->setFetchMode(PDO::FETCH_ASSOC);
+				$grade = $q3->fetch();
+				if (isset($grade['grade'])) {
+					$grade['grade'] = json_decode($grade['grade'],true);
+			?>
+				<tr>
+				<?php
+					$totalpoints = 0;
+		            foreach ($criteria as $key => $value) {
+		            	$value = get_object_vars($value);
+		            	$totalpoints+=$grade['grade'][$key]["point"];
+		            	?>
+		            <td>
+						<? echo $grade['grade'][$key]["point"];?>
+					</td>
+					<td>
+					<? echo $grade['grade'][$key]["comment"];?>
+		            </td>		            
+					<?php
+					}					
+					echo "<td>".$totalpoints."</td>";
+				} else {
+					echo "<td colspan='".(count($criteria)*2+1)."'>Ещё не проверено</td>";
+				}
+				?>
+	        	</tr>
+	        <?php
+	    	} else {
+			$q2=$pdodb->query('SELECT * from users WHERE group_id = "1";');
+			$q2->setFetchMode(PDO::FETCH_ASSOC);
+			$s=1;
+			while($student = $q2->fetch()) {
+			?>
+				<tr>
+				<?php
+				echo "<td>".$s."</td>";
+				echo "<td>".$student['username']."</td>";
+				$q3=$pdodb->query('SELECT * from grades WHERE task_id = "'.$task['task_id'].'" AND user_id = "'.$student['user_id'].'";');
+				$q3->setFetchMode(PDO::FETCH_ASSOC);
+				$grade = $q3->fetch();
+				if (isset($grade['grade'])) {
+					$grade['grade'] = json_decode($grade['grade'],true);
+					$totalpoints = 0;
+		            foreach ($criteria as $key => $value) {
+		            	$value = get_object_vars($value);
+		            	$totalpoints+=$grade['grade'][$key]["point"];
+		            	?>
+		            <td>
+						<? echo $grade['grade'][$key]["point"];?>
+					</td>
+					<td>
+					<? echo $grade['grade'][$key]["comment"];?>
+		            </td>		            
+					<?php
+					}					
+					echo "<td>".$totalpoints."</td>";
+				} else {
+					echo "<td colspan='".(count($criteria)*2+1)."'>Ещё не проверено</td>";
+				}
+				?>
+	        	</tr>
+	        <?php
+	        $s++;
+			}
+	    	}
+	        ?>
+        	</tbody>
+		</table>
+	<?php
+	}
+	?>
 </div>
 <?php
 require("footer.php");
